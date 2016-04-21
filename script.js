aaa769f328aa47a29f7ed2229f78d8dec0c5e17a
Mitchell Barton <mitchell@ringseven.com>
2016-04-21 11:47:45 -0600

    added close button

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 function mix() {
-  // var w = $(window);
-  // var lastPos = w.scrollTop();
-  // function poop() {
-  //   console.log('fire');
-  // };
 
   var amount = 1;
 
@@ -18,6 +13,24 @@ function mix() {
   });
 }
 
+function createClose() {
+  var newEl = document.createElement('div');
+  document.getElementsByTagName('body')[0].appendChild(newEl).className = "close-button";
+
+  var text = document.createElement('p');
+  var texttext = document.createTextNode('CLOSE');
+  text.appendChild(texttext);
+  newEl.appendChild(text);
+
+  setTimeout(function() {
+    $('.close-button').css('opacity', '1');
+  }, 1000);
+
+  $('.close-button').click(function() {
+    location.reload();
+  });
+}
+
 function blur() {
   $('body').wrapInner('<div class="blurry-container" />');
   setTimeout(function() {
@@ -25,12 +38,15 @@ function blur() {
     console.log('blurred');
     mix();
   }, 1000);
+
+
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "hello" ) {
       blur();
+      createClose();
     }
   }
 );
