79a805443f19024ebc7bf6457eca68e8c46e7099
Mitchell Barton <mitchell@ringseven.com>
2016-04-26 21:03:07 -0600

    adding tumblr stuff

--- script.js
+++ script.js
@@ -1,142 +1,10 @@
-/////// CONTENT SCRIPT
-
-var CAPTURE_DELAY = 150;
-
-function take() {
-  function onMessage(request, sender, callback) {
-      if (request.msg === 'scrollPage') {
-          getPositions(callback);
-      } else {
-          console.error('Unknown message received from background: ' + request.msg);
-      }
-  }
-
-  if (!window.hasScreenCapturePage) {
-      window.hasScreenCapturePage = true;
-      chrome.extension.onRequest.addListener(onMessage);
-  }
-
-  function max(nums) {
-      return Math.max.apply(Math, nums.filter(function(x) { return x; }));
-  }
-
-  function getPositions(callback) {
-      var body = document.body,
-          widths = [
-              document.documentElement.clientWidth,
-              document.body.scrollWidth,
-              document.documentElement.scrollWidth,
-              document.body.offsetWidth,
-              document.documentElement.offsetWidth
-          ],
-          heights = [
-              document.documentElement.clientHeight,
-              document.body.scrollHeight,
-              document.documentElement.scrollHeight,
-              document.body.offsetHeight,
-              document.documentElement.offsetHeight
-          ],
-          fullWidth = max(widths),
-          fullHeight = max(heights),
-          windowWidth = window.innerWidth,
-          windowHeight = window.innerHeight,
-          originalX = window.scrollX,
-          originalY = window.scrollY,
-          originalOverflowStyle = document.documentElement.style.overflow,
-          arrangements = [],
-          // pad the vertical scrolling to try to deal with
-          // sticky headers, 250 is an arbitrary size
-          scrollPad = 200,
-          yDelta = windowHeight - (windowHeight > scrollPad ? scrollPad : 0),
-          xDelta = windowWidth,
-          yPos = fullHeight - windowHeight,
-          xPos,
-          numArrangements;
-
-      // During zooming, there can be weird off-by-1 types of things...
-      if (fullWidth <= xDelta + 1) {
-          fullWidth = xDelta;
-      }
-
-      // Disable all scrollbars. We'll restore the scrollbar state when we're done
-      // taking the screenshots.
-      document.documentElement.style.overflow = 'hidden';
-
-      while (yPos > -yDelta) {
-          xPos = 0;
-          while (xPos < fullWidth) {
-              arrangements.push([xPos, yPos]);
-              xPos += xDelta;
-          }
-          yPos -= yDelta;
-      }
-
-      /** * /
-      console.log('fullHeight', fullHeight, 'fullWidth', fullWidth);
-      console.log('windowWidth', windowWidth, 'windowHeight', windowHeight);
-      console.log('xDelta', xDelta, 'yDelta', yDelta);
-      var arText = [];
-      arrangements.forEach(function(x) { arText.push('['+x.join(',')+']'); });
-      console.log('arrangements', arText.join(', '));
-      /**/
-
-      numArrangements = arrangements.length;
-
-      function cleanUp() {
-          document.documentElement.style.overflow = originalOverflowStyle;
-          window.scrollTo(originalX, originalY);
-      }
-
-      (function processArrangements() {
-          if (!arrangements.length) {
-              cleanUp();
-              if (callback) {
-                  callback();
-              }
-              return;
-          }
-
-          var next = arrangements.shift(),
-              x = next[0], y = next[1];
-
-          window.scrollTo(x, y);
-
-          var data = {
-              msg: 'capturePage',
-              x: window.scrollX,
-              y: window.scrollY,
-              complete: (numArrangements-arrangements.length)/numArrangements,
-              totalWidth: fullWidth,
-              totalHeight: fullHeight,
-              devicePixelRatio: window.devicePixelRatio
-          };
-
-          // Need to wait for things to settle
-          window.setTimeout(function() {
-              // In case the below callback never returns, cleanup
-              var cleanUpTimeout = window.setTimeout(cleanUp, 1250);
-
-              chrome.extension.sendRequest(data, function(captured) {
-                  window.clearTimeout(cleanUpTimeout);
-                  if (captured) {
-                      // Move on to capture next arrangement.
-                      processArrangements();
-                  } else {
-                      // If there's an error in popup.js, the response value can be
-                      // undefined, so cleanup
-                      cleanUp();
-                  }
-              });
-
-          }, CAPTURE_DELAY);
-      })();
-  }
-}
-
-
-////////////// MY STUFF
+/////// CONTENT SCRIPT  
 
 var body = document.getElementsByTagName('body')[0];
+
+function take() {
+  
+}
 
 function mix() {
 
@@ -145,7 +13,7 @@ function mix() {
   function change() {
     $('.blur').css('-webkit-filter', 'blur(' + amount + 'px)');
     amount = amount + .05;
-    console.log('more blur, amount = ' + amount);
+    // console.log('more blur, amount = ' + amount);
   }
 
   $(window).on('scroll', function() {
@@ -174,6 +42,7 @@ function createClose() {
     chrome.runtime.sendMessage({message: 'screenshot'}, function() {
       console.log('the close button was clicked, screenshot happening');
     });
+    // take();
   });
 }
 
@@ -193,9 +62,13 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "hello" ) {
       blur();
       createClose();
-    } else if ( request.message === "running" ) {
-      console.log('received running');
-      take();
+    } else if (request.message === "dataUrl") {
+      console.log('received data ' + dataUrl);
     }
+
+    // else if ( request.message === "running" ) {
+    //   console.log('received running');
+    //   take();
+    // }
   }
 );
