3eb07d1384c8e3501f4aae0b4d7e03e990467c0f
Mitchell Barton <mitchell@ringseven.com>
2016-05-03 17:23:20 -0600

    deleted oauth files

--- script.js
+++ script.js
@@ -13,37 +13,68 @@ function mix() {
   }
 
   $(window).on('scroll', function() {
-    change(.4);
+    change(.3);
   });
 
   $(window).on('mousemove', function() {
-    change(.15);
+    change(.05);
+  });
+}
+
+function addImg() {
+  var newImage = document.createElement('img');
+  body.appendChild(newImage).className = 'img-1';
+
+  // newImage.src = 'icon-2.png';
+  // $.get('http://pseudorandom-landscape.com/shapes', function() {
+  //   console.log('shapez');
+  // });
+
+  $.post('http://pseudorandom-landscape.com/shapes', 'i want images', function(data) {
+    alert(data);
   });
 }
 
 function createClose() {
-  ///create close button, and canvas elements
-  var newEl = document.createElement('div');
-  body.appendChild(newEl).className = "close-button";
+  //create close button, and canvas elements
+  // var newEl = document.createElement('div');
+  // body.appendChild(newEl).className = "close-button";
 
-  var text = document.createElement('p');
-  var texttext = document.createTextNode('CLOSE');
-  text.appendChild(texttext);
-  newEl.appendChild(text);
+  // var text = document.createElement('p');
+  // var texttext = document.createTextNode('CLOSE');
+  // text.appendChild(texttext);
+  // newEl.appendChild(text);
 
 
-
-  setTimeout(function() {
-    $('.close-button').css('opacity', '1');
-  }, 1000);
-
-  $('.close-button').click(function() {
-    // location.reload();
+  $(window).unload(function() {
     chrome.runtime.sendMessage({message: 'screenshot'}, function() {
       console.log('the close button was clicked, screenshot happening');
     });
-    // take();
+
+    // chrome.runtime.onMessage.addListener(
+    //   function(request, sender, sendResponse) {
+    //     if( request.message === "sent image" ) {
+    //       setTimeout(function() {
+    //         return console.log('finished');
+    //       }, 4000);
+          
+    //     }
+    //   }
+    // );
   });
+  // setTimeout(function() {
+  //   $('.close-button').css('opacity', '1');
+  // }, 1000);
+
+  // $()
+
+  // $('.close-button').click(function() {
+  //   // location.reload();
+  //   chrome.runtime.sendMessage({message: 'screenshot'}, function() {
+  //     console.log('the close button was clicked, screenshot happening');
+  //   });
+  //   // take();
+  // });
 }
 
 function blur() {
@@ -52,6 +83,7 @@ function blur() {
     $('.blurry-container').addClass('blur');
     console.log('blurred');
     mix();
+    addImg();
   }, 1000);
 
 
