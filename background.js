f6369a577ede23980db89f142e1e29d3924b842b
Mitchell Barton <mitchell@welikesmall.com>
2019-05-01 15:21:25 -0600

    updating

--- background.js
+++ background.js
@@ -4,32 +4,40 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-61480746-3']);
 _gaq.push(['_trackPageview']);
 
+var ga
+
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 
-function screenshot(e) {
-  chrome.tabs.captureVisibleTab(
-    null, {format: 'jpeg', quality: 100}, function(dataUrl) {
-      var date = Date.now();
-      var image = date + '-' + dataUrl;
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', 'https://pseudorandom-landscape.com/sand', true);
-      xhr.send(image);
-    });
+//
 
-  ga('send', 'event', 'sent and image = ' + image, 'screenshotted', 'BSWSGS');
+function screenshot(e) {
+  chrome.tabs.captureVisibleTab(null, {format: 'jpeg', quality: 100}, function(dataUrl) {
+    var date = Date.now();
+    var image = date + '-' + dataUrl;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://pseudorandom-landscape.com/sand', true);
+    xhr.send(image);
+
+    ga('send', 'event', 'sent and image = ' + image, 'screenshotted', 'BSWSGS');
+  });
 }
 
-///////////////////
+//
 
 function start() {
   chrome.alarms.create("yay", {
     delayInMinutes: 200,
     periodInMinutes: 240
   });
+
+  // chrome.alarms.create("yay", {
+  //   delayInMinutes: 1,
+  //   periodInMinutes: 2
+  // });
 }
 
 chrome.runtime.onMessage.addListener(
@@ -40,20 +48,17 @@ chrome.runtime.onMessage.addListener(
     }
 });
 
-function sandy() {
-
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "hello"}, function(response) {
-      console.log('i think it went');
-    });
-  });
-
-  chrome.alarms.getAll(function(alarms) {
-    console.log(alarms);
+function sandy(id) {
+  chrome.tabs.sendMessage(id, {message: "sandy"}, function(response) {
+    console.log('alarm went off - running sandy')
   });
 }
 
 chrome.runtime.onInstalled.addListener(start);
+chrome.alarms.onAlarm.addListener(function () {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
 
-chrome.alarms.onAlarm.addListener(sandy);
+    sandy(activeTab.id)
+  });
+});
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 ///// CONTENT SCRIPT
 
-var body = document.getElementsByTagName('body')[0];
+var body = document.body;
 var amount = 1;
 
 function mix(el, scroll, move) {
   function change(num) {
-    $(el).css('-webkit-filter', 'blur(' + amount + 'px)');
+    document.querySelector(el).style.WebkitFilter = 'blur(' + amount + 'px)'
     amount = amount + num;
   }
 
-  $(window).on('scroll', function() {
+  window.addEventListener('scroll', function() {
     change(scroll);
-  });
+  })
 
-  $(window).on('mousemove', function() {
+  window.addEventListener('mousemove', function() {
     change(move);
-  });
+  })
 }
 
 function addImg() {
@@ -24,31 +24,38 @@ function addImg() {
   var cls = 'shape img' + am;
   var mxcls = ".img" + am;
   body.appendChild(newImage).className = cls;
-  $.post('https://pseudorandom-landscape.com/shapes', 'give me images', function(data) {
-      newImage.src = data;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://pseudorandom-landscape.com/shapes', true)
+  xhr.send('give me images')
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      newImage.src = xhr.response;
       newImage.style.opacity = '.7';
-  });
+    }
+  }
 }
 
 var timer;
 var bl = 0;
 var nblur;
 
-function ch() {
-  nblur = $('.blur').css('-webkit-filter');
-  var fslice = nblur.indexOf('(') + 1;
-  var sslice = nblur.indexOf('px');
-  nblur = nblur.substring(fslice, sslice) * 1;
-
-  if (nblur > bl + 8) {
-    bl = nblur;
-    console.log('it worked ' + bl + ' ' + nblur);
-    addImg();
-    mix('.shape:nth-last-child(2)', 1.5, 1.5);
-  } else {
-    console.log('not blurry' + bl + ' ' + nblur);
-  }
-}
+// function ch() {
+//   nblur = $('.blur').css('-webkit-filter');
+//   var fslice = nblur.indexOf('(') + 1;
+//   var sslice = nblur.indexOf('px');
+//   nblur = nblur.substring(fslice, sslice) * 1;
+//
+//   if (nblur > bl + 8) {
+//     bl = nblur;
+//     console.log('it worked ' + bl + ' ' + nblur);
+//     addImg();
+//     mix('.shape:nth-last-child(2)', 1.5, 1.5);
+//   } else {
+//     console.log('not blurry' + bl + ' ' + nblur);
+//   }
+// }
 
 function shot() {
   chrome.runtime.sendMessage({message: 'screenshot'}, function() {
@@ -56,45 +63,57 @@ function shot() {
   });
 }
 
-function check() {
-  timer = setInterval(ch, 22000);
-}
+// function check() {
+//   timer = setInterval(ch, 22000);
+// }
 
 function createClose() {
-  $(document).keypress(function(e) {
+  document.addEventListener('keypress', function(e) {
+    var shape = document.querySelector('.shape');
+    var container = document.querySelector('.blurry-container')
+    var containerParent = container.parentNode
+
     if (e.keyCode === 83 || e.keyCode === 115) {
       shot();
-      console.log('pressed delete ' + e.keyCode);
-      $('.blurry-container').contents().unwrap();
-      $('.shape').remove();
-    } else if (e.keyCode === 67 || e.keycode === 99) {
-      console.log('pressed delete without save' + e.keyCode);
-      $('.blurry-container').contents().unwrap();
-      $('.shape').remove();
+      console.log('pressed delete! ' + e.keyCode);
+      shape.parentNode.removeChild(shape);
+      while (container.firstChild) containerParent.insertBefore(container.firstChild, container);
+      containerParent.removeChild(container);
+
+    } else if (e.keyCode === 99 || e.keycode === 67) {
+      console.log('pressed delete without save!' + e.keyCode);
+      shape.parentNode.removeChild(shape);
+      while (container.firstChild) containerParent.insertBefore(container.firstChild, container);
+      containerParent.removeChild(container);
     } else {
       console.log('pressed ' + e.keyCode);
     }
-  });
+  })
 
-  $(window).unload(function() {
+  window.addEventListener('beforeunload', function() {
     shot();
-  });
+  })
 }
 
 function blur() {
-  $('body').wrapInner('<div class="blurry-container" />');
+  var wrapper = document.createElement('div')
+  body.parentNode.insertBefore(wrapper, body);
+  wrapper.appendChild(body);
+  wrapper.className = 'blurry-container'
+
   setTimeout(function() {
-    $('.blurry-container').addClass('blur');
+    document.querySelector('.blurry-container').classList.add('blur');
     console.log('blurred');
     mix('.blur', 1.55, 1.55);
     addImg();
-    check();
+    // check();
   }, 1000);
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "hello" ) {
+    if( request.message === 'sandy' ) {
+      console.log('starting sandy')
       blur();
       createClose();
     } else if (request.message === "dataUrl") {
